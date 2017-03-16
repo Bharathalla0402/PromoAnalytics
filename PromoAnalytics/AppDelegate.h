47b0d4e4a3cccd65d1da486d59b0e9136767bc77
SwamyChunchu <chunchuswamy80@gmail.com>
2017-03-16 11:07:03 +0530

    Initial Commit

---
+++ PromoAnalytics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PromoAnalytics
+//
+//  Created by amit on 3/16/17.
+//  Copyright © 2017 Think360Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PromoAnalytics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PromoAnalytics
+//
+//  Created by amit on 3/16/17.
+//  Copyright © 2017 Think360Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
